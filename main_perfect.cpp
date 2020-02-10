dd010ddd81e1dca05257b815bc4c99362fb42072
Noah <noahsanchez73@gmail.com>
2020-02-10 22:41:54 +0000

    Actualizado

--- main_perfect.cpp
+++ main_perfect.cpp
@@ -45,17 +45,6 @@ vector<int> convertBinary(string s) {  // Palabra ASCII a binario
   return Bin;
 }
 
-vector<int> key(int size) {
-  vector<int> Key;
-  srand(time(NULL));
-
-  for (int i = 0; i < size; i++) {
-    Key.push_back(rand() % 2);
-  }
-
-  return Key;
-}
-
 vector<int> Sum(vector<int> a, vector<int> b) {
   vector<int> z;
   int aux = 0;
@@ -85,75 +74,58 @@ string BinaryStringToText(vector<int> a) {  // Pasamos binario a codigo ASCII
   return text;
 }
 
+void Print(string word, vector<int> a, vector<int> b, vector<int> c, vector<int> d) {
+  cout << word << "\t"
+       << " -> ";
+
+  for (int i = 0; i < a.size(); i++) cout << a[i] << " ";
+  cout << endl;
+  cout << "Clave"
+       << "\t"
+       << " -> ";
+
+  for (int i = 0; i < b.size(); i++) cout << b[i] << " ";
+  cout << endl;
+
+  cout << "Suma"
+       << "\t"
+       << " -> ";
+
+  for (int i = 0; i < c.size(); i++) cout << c[i] << " ";
+  cout << endl;
+
+  cout << "Codigo nuevo: " << BinaryStringToText(c) << endl;
+  cout << "Codigo anterior: " << BinaryStringToText(d) << endl;
+}
+
 int main() {
-  vector<int> y{1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0,
-                1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0};
-  vector<int> j{1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1};
+  vector<int> y{0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1,
+                1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1};
+  vector<int> j{0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1};
   bool w = 0;
 
   int Opc;
   cout << "Introduzca la dimension (2 o 3): ";
   cin >> Opc;
 
-  // Convertimos la palabra a binario
-
-  // Generando clave aleatoria
-  // vector<int> y = key(x.size());
-
   vector<int> suma;
   // Introducimos la palabra
   string Palabra;
   cout << "Introduzca la palabra: ";
   cin >> Palabra;
+
   vector<int> x = convertBinary(Palabra);
+  vector<int> Decod;
   switch (Opc) {
     case 2:
       suma = Sum(x, j);
-      cout << Palabra << "\t"
-           << " -> ";
-
-      for (int i = 0; i < x.size(); i++) cout << x[i] << " ";
-      cout << endl;
-      cout << "Clave"
-           << "\t"
-           << " -> ";
-
-      for (int i = 0; i < j.size(); i++) cout << y[i] << " ";
-      cout << endl;
-
-      cout << "Suma"
-           << "\t"
-           << " -> ";
-
-      for (int i = 0; i < suma.size(); i++) cout << suma[i] << " ";
-      cout << endl;
-
-      // Imprimimos la palabra nueva generada
-      cout << "Codigo nuevo: " << BinaryStringToText(suma) << endl;
+      Decod = Sum(suma, j);
+      Print(Palabra, x, j, suma, Decod);
       break;
     case 3:
       suma = Sum(x, y);
-      cout << Palabra << "\t"
-           << " -> ";
-
-      for (int i = 0; i < x.size(); i++) cout << x[i] << " ";
-      cout << endl;
-      cout << "Clave"
-           << "\t"
-           << " -> ";
-
-      for (int i = 0; i < y.size(); i++) cout << y[i] << " ";
-      cout << endl;
-
-      cout << "Suma"
-           << "\t"
-           << " -> ";
-
-      for (int i = 0; i < suma.size(); i++) cout << suma[i] << " ";
-      cout << endl;
-
-      // Imprimimos la palabra nueva generada
-      cout << "Codigo nuevo: " << BinaryStringToText(suma) << endl;
+      Decod = Sum(suma, y);
+      Print(Palabra, x, y, suma, Decod);
       break;
     default:
       cout << "Introduzca 2 o 3" << endl;
